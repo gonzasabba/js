d85ddd350ea79513e94c6635d8d48f3c4c7d679f
Gonzalo Sabbatini <gonzasabba@gmail.com>
2023-03-21 02:45:33 -0300

    2da entrega

--- index.js
+++ index.js
@@ -1,13 +1,40 @@
 
-const cantidadPersonas = prompt("Ingrese la cantidad de personas:");
+const monto = Number(prompt('Ingrese el monto del préstamo:'));
+const plazo = Number(prompt('Ingrese el plazo del préstamo en meses (3, 6, 9, 12):'));
 
-let sumaEdades = 0;
+let interes = 0;
+let montoConInteres = monto;
 
-for (let i = 0; i < cantidadPersonas; i++) {
-    const edad = parseInt(prompt(`Ingrese la edad de la persona ${i+1}:`));
-    sumaEdades += edad;
+if (monto < 10000) {
+  interes = 0.1;
+} else if (monto >= 10001 && monto <= 49999) {
+  interes = 0.15;
+} else {
+  interes = 0.2;
 }
 
-const promedioEdades = sumaEdades / cantidadPersonas;
+switch (plazo) {
+  case 3:
+    montoConInteres = monto * (1 + interes) ** 3;
+    break;
+  case 6:
+    montoConInteres = monto * (1 + interes) ** 6;
+    break;
+  case 9:
+    montoConInteres = monto * (1 + interes) ** 9;
+    break;
+  case 12:
+    montoConInteres = monto * (1 + interes) ** 12;
+    break;
+  default:
+    alert('Plazo ingresado incorrecto.');
+}
 
-alert(`La edad promedio de las ${cantidadPersonas} personas registradas es ${promedioEdades}.`);
+const resultados = [
+  `Monto del préstamo: $${monto}`,
+  `Interés aplicado: ${interes * 100}%`,
+  `Plazo del préstamo: ${plazo} meses`,
+  `Monto con interés: $${montoConInteres}`
+];
+
+console.log(resultados);
